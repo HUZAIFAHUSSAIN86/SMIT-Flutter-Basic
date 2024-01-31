473239ab7c071a88b093421f5e7419161ccad4d6
HUZAIFAHUSSAIN86 <huzaifahussain707@gmail.com>
2024-01-31 14:22:33 +0500

    list code

--- 09-Program Even & Odd.dart
+++ 09-Program Even & Odd.dart
@@ -1,10 +1,21 @@
-void main (){
-  int num = 8;
+// void main (){
+//   int num = 8;
 
-  if( num .isOdd){
-    print("This number is : Odd");
-  }
-  else{
-print("This number is : Even");
-  }
-}
+//   if( num .isOdd){
+//     print("This number is : Odd");
+//   }
+//   else{
+// print("This number is : Even");
+//   }
+// }
+
+ void main(){
+  int number = 3;
+
+ if(number % 2 ==0){
+  print("This Number is: Even");
+ }else if(number % 5 == 1){
+  print("This Number is: Odd");
+ }
+
+ }
---
+++ List/Simple-list.dart
@@ -0,0 +1,5 @@
+//-------------------------------------------------------Simple list code (01)
+   void main(){
+   List<String>  list = ['Huzaifa' ,'Hussain', 'Ahmed'];
+   print(list);
+  }
