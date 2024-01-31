a9a027424e80cf2e9ce9a164a0f31162fef88964
HUZAIFAHUSSAIN86 <huzaifahussain707@gmail.com>
2024-01-31 14:31:42 +0500

    Assignment.dart

--- 09-Program Even & Odd.dart
+++ 09-Program Even & Odd.dart
@@ -1,21 +1,23 @@
-// void main (){
-//   int num = 8;
+//----------------------------------------Method (1)
+void main (){
+  int num = 9;
 
-//   if( num .isOdd){
-//     print("This number is : Odd");
-//   }
-//   else{
-// print("This number is : Even");
-//   }
-// }
+  if( num .isOdd){
+    print("This number is : Odd");
+  }
+  else{
+print("This number is : Even");
+  }
+}
 
- void main(){
-  int number = 3;
+//----------------------------------------Method (2)
+//  void main(){
+//   int number = 3;
 
- if(number % 2 ==0){
-  print("This Number is: Even");
- }else if(number % 5 == 1){
-  print("This Number is: Odd");
- }
+//  if(number % 2 ==0){
+//   print("This Number is: Even");
+//  }else if(number % 5 == 1){
+//   print("This Number is: Odd");
+//  }
 
- }
+//  }
