aaf85d37da05ff542a619c1ca4c82ffcf6ce7870
HUZAIFAHUSSAIN86 <huzaifahussain707@gmail.com>
2024-02-02 23:39:05 +0500

    list code

---
+++ List/Empty list & add.dart
@@ -0,0 +1,13 @@
+
+//-------------------------------------Empty list & add data 
+ void main() {
+ var myList = [];
+ 
+ myList.add(25);
+ 
+ myList.add(63);
+ 
+ myList.add(84);
+ 
+ print(myList);
+}
