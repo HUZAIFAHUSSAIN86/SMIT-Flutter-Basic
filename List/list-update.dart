c7c71d613a177ba5dab6c4c91b11303351b31c59
HUZAIFAHUSSAIN86 <huzaifahussain707@gmail.com>
2024-02-10 13:03:07 +0500

    list code

---
+++ List/list-update.dart
@@ -0,0 +1,10 @@
+//------------------------------------------------------- List update
+
+void main(){
+List<String> fruits = ["Apple", "Mango", "Banana"];
+
+//The index of Mango is 1
+
+fruits[1] = "HUZAIFA"; 
+print(fruits);
+}
