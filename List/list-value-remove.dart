d707d4f85a8c56772d5043cbcc5dda6eef016d3a
HUZAIFAHUSSAIN86 <huzaifahussain707@gmail.com>
2024-02-12 19:47:29 +0500

    list code

---
+++ List/list-value-remove.dart
@@ -0,0 +1,10 @@
+
+//---------------------------------------------------list value remove
+
+ void main(){
+ List< String> list = ['huzaifa' ,'hussain'];
+
+ list.remove('huzaifa');
+  
+ print(list);
+}
