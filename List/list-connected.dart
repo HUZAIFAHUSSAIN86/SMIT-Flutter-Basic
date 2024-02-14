e6872dffeb7e5c3345279a9adeb5819d09a768bc
HUZAIFAHUSSAIN86 <huzaifahussain707@gmail.com>
2024-02-14 14:13:09 +0500

    list code

---
+++ List/list-connected.dart
@@ -0,0 +1,13 @@
+//---------------------------------------------- variable to variable connect (07)
+  void main(){
+    List list1 = [24, 'Hello', 84];
+    List list2 = [41, 65];
+    
+    print(list1);
+  
+  //join list2 to list1
+  
+   list1.addAll(list2);
+     
+   print(list1);
+}
