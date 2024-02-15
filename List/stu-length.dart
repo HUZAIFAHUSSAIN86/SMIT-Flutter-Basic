6d8774de23f86b238d2354d8ebb4a89e1aae3444
HUZAIFAHUSSAIN86 <huzaifahussain707@gmail.com>
2024-02-15 13:28:58 +0500

    list code

---
+++ List/stu-length.dart
@@ -0,0 +1,8 @@
+
+//----------------------------------------(list)-Find the student length
+
+void main(){
+  List students = ["huzaifa", "ahmed", "haider" , "hamza"];
+  print(students.length);
+}
+
