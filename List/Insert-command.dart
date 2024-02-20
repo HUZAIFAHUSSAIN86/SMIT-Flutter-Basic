46527ad70f1f6a195381e0971d6878e3dc6ca210
HUZAIFAHUSSAIN86 <huzaifahussain707@gmail.com>
2024-02-20 15:19:49 +0500

    list code

--- List/Insert-command.dart
+++ List/Insert-command.dart
@@ -1,10 +1,10 @@
 //-----------------------------------------------------------insert command (04)
-void main(){
-  
- List list = [25,26,29,55,100];
- print(list);
-  
- list.insert(0,10000);
- print(list);
-    
+void main() {
+  List list = [25, 26, 29, 55, 100];
+  print(list);
+
+  // first index then value
+
+  list.insert(0, 10000);
+  print(list);
 }
