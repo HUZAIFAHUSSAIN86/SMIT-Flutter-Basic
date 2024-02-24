2310e4a512049ac31280fd4512b91cd7c0f8d70a
HUZAIFAHUSSAIN86 <huzaifahussain707@gmail.com>
2024-02-24 15:00:05 +0500

    list code

--- List/add-method.dart
+++ List/add-method.dart
@@ -1,10 +1,9 @@
-
 //-------------------------------------------------------Add value to list (02)
-void main(){
- var mylist = [25, 63, 84, 80];  
-print(mylist);
-     
+void main() {
+  var mylist = [25, 63, 84, 80, 92];
+  print(mylist);
+
 //add number to list
- mylist.add(96);
-print(mylist);
+  mylist.add(96);
+  print(mylist);
 }
