a311279f54ce98e12d7e650cf8fe8c9eef58bd22
HUZAIFAHUSSAIN86 <huzaifahussain707@gmail.com>
2024-02-28 16:30:27 +0500

    list code

--- List/Simple-list.dart
+++ List/Simple-list.dart
@@ -1,5 +1,5 @@
 //-------------------------------------------------------Simple list code (01)
-   void main(){
-   List<String>  list = ['Huzaifa' ,'Hussain', 'Ahmed'];
-   print(list);
-  }
+void main() {
+  List<String> list = ['Huzaifa', 'Hussain', 'Ahmed', 'Bilal'];
+  print(list);
+}
